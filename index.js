4e139bb5f41c636ac70f666cf4dc523ee55d5468
alen-sliit <it24103473@my.sliit.lk>
2024-08-14 22:09:42 +0530

    ok

--- index.js
+++ index.js
@@ -1,34 +1,90 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+var NumofMed = 8;
+var userName ="Alen"; 
+var userEmail ="alen@gmail.com";
+var addedMedi = [];
+var mediInfo = {"1":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"08:00",
+                  },
+                  "2":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"10:00",
+                  },
+                  "3":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"08:00",
+                  },
+                  "4":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"10:00",
+                  },
+                  "5":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"12:00",
+                  },
+                  "6":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"10:00",
+                  },
+                  "7":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"08:00",
+                  },
+                  "8":
+                   {"medicineStatus":"pending",
+                    "medicineMom":"before",
+                    "medicineTime":"10:00",
+                  }               
+                };
+
 app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.json()); 
+
+app.use(express.static("public"));
+
+function dashboardRen(res){
+  res.render("index.ejs",{
+    medNum: NumofMed,
+    userEmail:userEmail,
+    userName:userName,
+    addedMedi:addedMedi,
+    mediInfo:mediInfo,
+  });
+}
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  dashboardRen(res);
 });
 
-app.post("/register", (req, res) => {
-  //Do something with the data
-  res.sendStatus(201);
+app.post("/saveData", (req, res,) => {
+  addedMedi.push(req.body["medi-name"]);
+  mediInfo[req.body["medi-name"]]["medicineMom"]=req.body["meal-mom"];
+  mediInfo[req.body["medi-name"]]["medicineTime"]=req.body["reminder-time"]
+  res.redirect("/");
 });
 
-app.put("/user/angela", (req, res) => {
-  res.sendStatus(200);
+app.post("/remTime", (req, res,) => {
+  addedMedi = addedMedi.filter(addedMedi => addedMedi !== req.body.data);
+  res.redirect("/");
 });
 
-app.patch("/user/angela", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.delete("/user/angela", (req, res) => {
-  //Deleting
-  res.sendStatus(200);
+app.post("/userData",(req,res)=>{
+    userName = req.body["fname"];
+    userEmail = req.body["email"];
+    res.redirect("/");
 });
 
 app.listen(port, () => {
---
+++ script.js
@@ -0,0 +1,4 @@
+<% document.getElementById("remMedi").addEventListener("click", myFunction);%>
+<% console.log("You clicked me");%>
+<% function myFunction() {%>
+<% console.log("You clicked me");}%>
