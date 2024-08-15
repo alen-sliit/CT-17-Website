a341c182670d80793d7397e0f0652ce4daaf67cf
alen-sliit <it24103473@my.sliit.lk>
2024-08-15 22:34:32 +0530

    ok

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3000;
 
 var NumofMed = 8;
-var userName ="Alen"; 
-var userEmail ="alen@gmail.com";
+var userName = null; 
+var userEmail = null;
 var addedMedi = [];
 var mediInfo = {"1":
                    {"medicineStatus":"pending",
@@ -51,7 +51,7 @@ var mediInfo = {"1":
                 };
 
 app.use(bodyParser.urlencoded({ extended:true }));
-app.use(express.json()); 
+app.use(express.json());
 
 app.use(express.static("public"));
 
@@ -66,17 +66,28 @@ function dashboardRen(res){
 }
 
 app.get("/", (req, res) => {
+  res.header("Refresh", "5");
   dashboardRen(res);
 });
 
 app.post("/saveData", (req, res,) => {
-  addedMedi.push(req.body["medi-name"]);
-  mediInfo[req.body["medi-name"]]["medicineMom"]=req.body["meal-mom"];
+  var isthere = true;
+
+  for (let i = 0; i < 8; i++){
+    if (addedMedi[i] == req.body["medi-name"]){
+      isthere = false;
+    }
+  }
+  if (isthere){
+    addedMedi.push(req.body["medi-name"]);
+    mediInfo[req.body["medi-name"]]["medicineMom"]=req.body["meal-mom"];
+  }
   mediInfo[req.body["medi-name"]]["medicineTime"]=req.body["reminder-time"]
   res.redirect("/");
 });
 
 app.post("/remTime", (req, res,) => {
+  mediInfo[req.body.data]["medicineStatus"] = "pending";
   addedMedi = addedMedi.filter(addedMedi => addedMedi !== req.body.data);
   res.redirect("/");
 });
@@ -85,6 +96,21 @@ app.post("/userData",(req,res)=>{
     userName = req.body["fname"];
     userEmail = req.body["email"];
     res.redirect("/");
+});
+
+app.get("/espGet", (req, res) => {
+  var sendMediList = {};
+  for (let i = 0; i < addedMedi.length; i++){
+    sendMediList[addedMedi[i]] = mediInfo[addedMedi[i]];
+  }
+  res.send(sendMediList);
+});
+
+app.post("/espPost", (req, res) => {
+  for (let i = 0; i < Object.values(req.body).length; i++){
+    mediInfo[Object.keys(req.body)[i]]["medicineStatus"] = Object.values(req.body)[i];
+  }
+  res.status(200).json({ message: "Received Data" });
 });
 
 app.listen(port, () => {
